9dfe97181d81b57a2c5bb6d1b24fb446190a7f02
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-07-08 18:00:45 -0500

    primer commit

---
+++ cmake-build-debug/CMakeFiles/Boletin_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Boletin_1"
+  "Boletin_1.pdb"
+  "CMakeFiles/Boletin_1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Boletin_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
